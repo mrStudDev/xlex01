c6362b4f4e05cbe8e06665f5a5bf3cd813e1415c
mrStudDev <marcosscfloripa@gmail.com>
2024-03-06 19:13:24 -0300

    def

--- xlexapp/settings.py
+++ xlexapp/settings.py
@@ -162,3 +162,4 @@ MEDIA_URL = '/media/'
 # Default primary key field type 
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
