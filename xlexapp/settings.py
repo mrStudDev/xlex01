40810938039152dc1511123a3cfb1cf76c663fd4
mrStudDev <marcosscfloripa@gmail.com>
2024-03-06 16:57:45 -0300

    ck erro

--- xlexapp/settings.py
+++ xlexapp/settings.py
@@ -164,5 +164,3 @@ CKEDITOR_UPLOAD_PATH = "uploads/images/"
 # Default primary key field type 
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
