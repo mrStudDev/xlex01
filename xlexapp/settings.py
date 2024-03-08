3de538d8a7624eecc743e38b868a3866c255ecbe
mrStudDev <marcosscfloripa@gmail.com>
2024-03-08 01:41:56 -0300

    staticfiles ch

--- xlexapp/settings.py
+++ xlexapp/settings.py
@@ -154,7 +154,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
